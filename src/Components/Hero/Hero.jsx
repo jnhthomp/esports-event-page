1300e3d671a18be1effec3caa55747823d69dab2
Jonah <jnh.thompson@gmail.com>
2023-08-18 10:04:24 -0700

    Add Hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <div className={classes.heroContainer}></div>
+  )
+}
+
+export default Hero
