a3d10ba84618e694b830cb7f256f17acbacd32dd
Jonah Thompson <jnh.thompson@gmail.com>
2023-08-23 19:54:30 -0700

    Update TODOs

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   );
 }
 
-// TODO: Move Button component from navbar/hero into own component
 // TODO: Move title components out of <Hero/> and nest them as child components instead
 // TODO: Add color vars for a more unified color scheme and update existing colors to theme
 //       - This will also make it easier to adjust the theme of the site for different use cases
@@ -19,5 +18,8 @@ function App() {
 // TODO: Add styling to <ListItem />
 // TODO: Remove RaceIcon.svg: Consider making this a component so it is easy to apply styles in css and call as needed
 //       - If ths is done be sure to insert this icon into the navbar 
+// TODO: In slidelist transition from useState to useReducer
+// TODO: In slidelist try to use only 1 useReft and search for children to find elements that are needed instead of having nested refs
+// TODO: Add onClick method to <ScheduleButton /> (will be used after schedule modal is created)
 
 export default App;
