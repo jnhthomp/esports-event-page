86bca55e6987fd74f8bbcabbd8b657ec9b1259c2
Jonah <jnh.thompson@gmail.com>
2023-08-23 21:15:55 -0700

    Add details and style to <ListItem /> components

--- src/Components/SlideList/ListItem/ListItem.jsx
+++ src/Components/SlideList/ListItem/ListItem.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import classes from './ListItem.module.css'
 
-const ListItem = () => {
+const ListItem = (props) => {
   return (
-    <div className={classes.listItem}>Item</div>
+    <div className={classes.listItem}>
+      {/* Date/Time */}
+      <span className={classes.date}>{props.eventDetails.date}</span>
+      <span className={classes.time}>{props.eventDetails.time}</span>
+      {/* Series + Round*/}
+      <span>{props.eventDetails.series}: Round {props.eventDetails.round}</span>
+      {/* Track */}
+      <span>{props.eventDetails.circuit}</span>
+    </div>
   )
 }
 
