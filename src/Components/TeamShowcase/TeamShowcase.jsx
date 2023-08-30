e67d118c21f4b52064eff55bd008674b0252eb2f
Jonah Thompson <jnh.thompson@gmail.com>
2023-08-30 08:41:29 -0700

    Generate li elements dynamically

--- src/Components/TeamShowcase/TeamShowcase.jsx
+++ src/Components/TeamShowcase/TeamShowcase.jsx
@@ -2,32 +2,43 @@ import React from 'react';
 import classes from './TeamShowcase.module.css';
 
 const TeamShowcase = () => {
+  // Generate all the teams for the team showcase
+  // Add variety of colors by adding a color property to each object and using that to assign a classname to the generated li
+  const teams = [
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'},
+    {name: 'Team Name'}
+  ]
 
+  const teamLI = teams.map((el, i) => {
+    return (
+      <li key={i}>{el.name}</li>
+    )
+  }
+  )
   return (
     <div className={classes.showcaseContainer}>
       <div className={classes.title}>Teams</div>
       <div>
         <ul>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
-          <li>Team Name</li>
+          {teamLI}
         </ul>
       </div>
     </div>
